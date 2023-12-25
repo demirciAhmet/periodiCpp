73efbfd704710ca73535154eef6a7da5eebc08e5
Mehmet Emre Cebeci <135210010+mecebeci@users.noreply.github.com>
2023-12-25 18:18:04 +0300

    Delete STAGE4/helium.cpp

--- STAGE4/helium.cpp
+++
@@ -1,9 +0,0 @@
-#include "helium.h"
-
-Helium::Helium( QString name,  double atomicMass,  QString electronicConfiguration, QObject *parent)
-    : QObject{parent}
-{
-    this->name = name;
-    this->atomicMass = atomicMass;
-    this->electronicConfiguration = electronicConfiguration;
-}
