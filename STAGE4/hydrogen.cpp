e48597dcd2cc29d4d862222566781aa8d6f2a878
Mehmet Emre Cebeci <135210010+mecebeci@users.noreply.github.com>
2023-12-25 18:18:47 +0300

    Delete STAGE4/hydrogen.cpp

--- STAGE4/hydrogen.cpp
+++
@@ -1,9 +0,0 @@
-#include "hydrogen.h"
-
-Hydrogen::Hydrogen( QString name,  double atomicMass,  QString electronicConfiguration, QObject *parent)
-    : QObject{parent}
-{
-    this->name = name;
-    this->atomicMass = atomicMass;
-    this->electronicConfiguration = electronicConfiguration;
-}
